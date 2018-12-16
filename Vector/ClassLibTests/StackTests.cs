d9f74b756e03f66113cef197da86d350eeab6b01
sir Lenko <lenko.studio@gmail.com>
2018-12-16 16:57:09 +0700

    Создание шаблонов тестов

---
+++ Vector/ClassLibTests/StackTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib.Tests
+{
+    [TestClass()]
+    public class StackTests
+    {
+        [TestMethod()]
+        public void ClearTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PushTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PullTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TopTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsEmptyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsFullTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Vector/ClassLibTests/VectorTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib.Tests
+{
+    [TestClass()]
+    public class VectorTests
+    {
+        [TestMethod()]
+        public void PushBackTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsEmptyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SizeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EraseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PopBackTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
