6388454597f3357ce798d573bb91e628ed1924c6
sir Lenko <lenko.studio@gmail.com>
2018-12-16 16:28:47 +0700

    Клонирование

---
+++ Vector/ClassLib/Stack.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+
+namespace ClassLib
+{
+    class Stack
+    {
+        /// <summary>
+        /// Создается массив векторов
+        /// </summary>
+        private Vector[] _elem;
+        private int _size;
+
+        /// <summary>
+        /// Количество элементов в стеке
+        /// </summary>
+        private int _count;
+        
+        /// <summary>
+        /// Конструктор, создает стек заданной длинны
+        /// </summary>
+        /// <param name="size">Длинна стека</param>
+        public Stack(int size)
+        {
+			_count = 0;
+			if (size > 0)
+			{
+				_size = size;
+				_elem = new Vector[_size];
+			}
+
+        }
+
+        /// <summary>
+        /// Очищает стек
+        /// </summary>
+        public void Clear()
+        {
+            _size = 0;
+            _count = 0;
+            Array.Clear(_elem, 0, _elem.Count()-1);
+        }
+
+        /// <summary>
+        /// Добавляет вектор в стек
+        /// </summary>
+        /// <param name="vector">Вектор, который нужно добавить</param>
+        public void Push (Vector vector)
+        {
+            if (_count >= _size)
+            {
+                return;
+            }
+            _elem[_count] = vector;
+            _count++;
+        }
+
+        /// <summary>
+        /// Взять верхний вектор из стека и удалить его оттуда 
+        /// </summary>
+        /// <returns>Вектор который находится на вершине стека</returns>
+        public Vector Pull ()
+        {
+            if (_count == 0)
+                return null;
+            Vector temp =  _elem[_count-1];
+            Array.Clear(_elem, _count-1, _count-1);
+            _count--;
+            return temp;
+            
+        }
+
+        /// <summary>
+        /// Получаем вектор с вершины стека, но не удаляем
+        /// </summary>
+        /// <returns>Вектор на вершине стека</returns>
+        public Vector Top ()
+        {
+            if (_count == 0)
+            {
+                return null;
+            }
+            return _elem[_count-1];
+        }
+
+        /// <summary>
+        /// Проверяет, пустой ли стек
+        /// </summary>
+        /// <returns>True если стек пустой, False если в стеке есть вектора</returns>
+        public bool IsEmpty()
+        {
+            if (_count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Проверяет, полный ли стек
+        /// </summary>
+        /// <returns>True если стек полнстью заполнен, false если не полностью</returns>
+        public bool IsFull()
+        {
+            if (_size == _count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Vector/ClassLib/Vector.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLib
+{
+    public class Vector
+    //оператор IErase, 
+    {
+        /// <summary>
+        /// Создаем контейнер для хранения чисел
+        /// </summary>
+        private List<int> elems;
+
+        /// <summary>
+        /// Оператор для обращения к элементу вектора
+        /// </summary>
+        public int this[int i]
+		{
+			get
+			{
+				try
+				{
+					return elems[i];
+				}
+				catch (ArgumentOutOfRangeException outOfRange)
+				{
+
+					Console.WriteLine("Error: {0}", outOfRange.Message);
+					return -0;  //нужно что то вернуть еще, ругается если не возврщаться в этом блоке
+				}
+			}
+		}
+
+        /// <summary>
+        /// Конструктор. При вызове создает динамичный контейнер
+        /// </summary>
+        public Vector() {
+            elems = new List<int>();
+        }
+
+        /// <summary>
+        /// Добавляет значение в "вектор"
+        /// </summary>
+        /// <param name="value">Значение которое нужно добавить в "вектор"</param>
+        public void PushBack(int value)
+        {
+            elems.Add(value);
+        }
+
+        /// <summary>
+        /// Проверяет, пустой ли вектор
+        /// </summary>
+        /// <returns>Возвращает true если пустой, false если есть хотя бы один элемент</returns>
+        public bool IsEmpty()
+        {
+            return (elems.Count() == 0);
+        }
+
+        /// <summary>
+        /// Возвращает количество элементов в "векторе"
+        /// </summary>
+        /// <returns></returns>
+        public int Size()
+        {
+            return elems.Count();
+        }
+
+        /// <summary>
+        /// Очищает "вектор"
+        /// </summary>
+        public void Clear()
+        {
+            elems.Clear();
+        }
+
+        /// <summary>
+        /// Добавляет число в конкретную позицию в "векторе"
+        /// </summary>
+        /// <param name="pos">Позиция в "векторе"</param>
+        /// <param name="value">Значение, которое нужно добавить</param>
+        public void Insert(int pos, int value)
+        {
+			if (pos > 0)
+			{
+				if (elems.Count() >= pos)
+					elems.Insert(pos, value);
+			}
+        }
+
+        /// <summary>
+        /// Удаляет число в нужно позиции
+        /// </summary>
+        /// <param name="pos">Позиция числа, которое нужно удалить</param>
+        public void Erase(int pos)
+        {
+			if (pos > 0)
+			{
+				if (elems.Count() >= pos)
+					elems.Remove(elems[pos]);
+			}
+        }
+
+        /// <summary>
+        /// Удаляет последний элемент
+        /// </summary>
+        public void PopBack()
+        {
+            elems.RemoveAt(elems.Count-1);
+        }
+    }
+}
